fb0f79741e8a97533db250bf1122955fac6a682a
yzhedwin <zizo_edwin@hotmail.com>
2025-05-30 16:26:59 +0800

    cleanup unused web api function

--- src/main/java/com/demo/openapi/handler/web/WebAPIHandler.java
+++ src/main/java/com/demo/openapi/handler/web/WebAPIHandler.java
@@ -1,35 +1,18 @@
 package com.demo.openapi.handler.web;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.webflux.outbound.WebFluxRequestExecutingMessageHandler;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import com.demo.openapi.config.web.WebClientConfig;
 import com.demo.openapi.model.GenericMessageModel;
 import com.demo.openapi.model.SchedulerModel;
 import com.demo.openapi.service.WebService;
 
 @Service
 public class WebAPIHandler {
-  WebClientConfig config;
   WebService webService;
 
-  public WebAPIHandler(WebClientConfig config, WebService webService) {
-    this.config = config;
+  public WebAPIHandler(WebService webService) {
     this.webService = webService;
-  }
-
-  @ServiceActivator(inputChannel = "apiRequestChannel")
-  @Bean
-  public WebFluxRequestExecutingMessageHandler reactiveOutbound(WebClient client) {
-    WebFluxRequestExecutingMessageHandler handler = new WebFluxRequestExecutingMessageHandler(
-        config.getBaseUrl() + "/foo", client);
-    handler.setHttpMethod(HttpMethod.POST);
-    handler.setExpectedResponseType(String.class);
-    return handler;
   }
 
   @ServiceActivator(inputChannel = "apiRequestChannel")
@@ -44,4 +27,16 @@ public class WebAPIHandler {
       default -> System.out.println("Received message in WebClientHandler: " + message);
     }
   }
+
+  // @ServiceActivator(inputChannel = "apiRequestChannel")
+  // @Bean
+  // public WebFluxRequestExecutingMessageHandler reactiveOutbound(WebClient
+  // client) {
+  // WebFluxRequestExecutingMessageHandler handler = new
+  // WebFluxRequestExecutingMessageHandler(
+  // config.getBaseUrl() + "/foo", client);
+  // handler.setHttpMethod(HttpMethod.POST);
+  // handler.setExpectedResponseType(String.class);
+  // return handler;
+  // }
 }
